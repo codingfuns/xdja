6e1efada75b532129d89126058ee7e11e1c244e5
unknown <wujing@xdja.com>
2020-03-18 16:26:21 +0800

    cli

---
+++ command/delete.js
@@ -0,0 +1,38 @@
+/*
+ * @Description: file content
+ * @Author: 
+ * @Date: 2020-03-18 16:22:08
+ * @LastEditors: wujing
+ * @LastEditTime: 2020-03-18 16:24:36
+ */
+'use strict'
+const co = require('co')
+const prompt = require('co-prompt')
+const config = require('../template')
+const chalk = require('chalk')
+const fs = require('fs')
+
+module.exports = () => {
+    co(function *() {
+        // 接收用户输入的参数
+        let tplName = yield prompt('Template name: ')
+
+        // 删除对应的模板
+        if (config.tpl[tplName]) {
+            config.tpl[tplName] = undefined
+        } else {
+            console.log(chalk.red('Template does not exist!'))
+            process.exit()
+        }
+        
+        //写入template.json
+        fs.writeFile(__dirname + '/../template.json', JSON.stringify(config),     'utf-8', (err) => {
+            if (err) console.log(err)
+            console.log(chalk.green('Template deleted!'))
+            console.log(chalk.grey('The last template list is: \n'))
+            console.log(config)
+            console.log('\n')
+            process.exit()
+        })
+    })
+}
---
+++ command/list.js
@@ -0,0 +1,14 @@
+/*
+ * @Description: file content
+ * @Author: 
+ * @Date: 2020-03-18 16:22:08
+ * @LastEditors: wujing
+ * @LastEditTime: 2020-03-18 16:24:52
+ */
+'use strict'
+const config = require('../template')
+
+module.exports = () => {
+     console.log(config.tpl)
+     process.exit()
+}
